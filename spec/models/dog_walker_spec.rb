d84fabc0ca9f7c54cd25c03d18e8837706493794
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-15 14:10:06 -0500

    add dog_walker_spec.rb

--- spec/models/dog_walker_spec.rb
+++ spec/models/dog_walker_spec.rb
@@ -4,15 +4,15 @@ describe DogWalker do
 
   describe "::with_preferred_customers" do
     it "returns all dog walkers who walk dogs for preferred customers" do
-      dog_walkers = []
-      expect(DogWalker.with_preferred_customers.pluck(:name).uniq).to eq(dog_walkers)
+      dog_walkers = ["Dwight Schrute", "Angela Martin", "Phyllis Vance", "Stanley Hudson", "Ryan Howard", "Oscar Martinez"]
+      expect(DogWalker.with_preferred_customers.pluck(:name).sort).to eq(dog_walkers.sort)
     end
   end
 
-  describe "::method" do
-    it "" do
-      dog_walkers = []
-      expect(DogWalker.method.pluck(:name)).to eq(dog_walkers)
+  describe "::with_big_dogs" do
+    it "returns all dog walkers who walk dogs weighing 50 pounds or more" do
+      dog_walkers = ["Jim Halpert", "Pam Beasley", "Dwight Schrute", "Michael Scott", "Kelly Kapoor", "Ryan Howard", "Kevin Malone"]
+      expect(DogWalker.with_big_dogs.pluck(:name)).to eq(dog_walkers)
     end
   end
 end
