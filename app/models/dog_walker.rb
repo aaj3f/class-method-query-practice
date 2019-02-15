17e7f9692ce9dba1c0b3c78ad680c0b10555ba64
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-15 17:07:20 -0500

    add solutions

--- app/models/dog_walker.rb
+++ app/models/dog_walker.rb
@@ -2,4 +2,12 @@ class DogWalker < ApplicationRecord
   belongs_to :dog_walking_company
   has_many :dogs
   has_many :dog_owners, through: :dogs
+
+  def self.with_preferred_customers
+    joins(dogs: :dog_owner).group("dog_walkers.id").having(dog_owners: { preferred_customer: true } )
+  end
+
+  def self.with_big_dogs
+    joins(:dogs).group("dog_walkers.id").having("dogs.weight >= 50")
+  end
 end
